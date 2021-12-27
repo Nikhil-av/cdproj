dfaea1b4d2a66cfb54409b2dd6b3b755953f47e0
nikhil <nikhilsaid@gmail.com>
2021-12-27 22:29:31 +0530

    comm

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const port = process.env.PORT || 3500
 const path = require("path")
 app.listen(port, () => console.log(`server on ${port}...`))
 app.use(exp.static(path.join(__dirname, './dist/cdconcepts/')))
+app.use((req, res, next) => {
+
+    res.send({ message: `path ${req.url} is invalid` })
+})
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    res.send({ message: `error is ${err.message}` })
+})
